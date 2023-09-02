cb1b34e30e08ef60b8e5a1babab46500b3a5a55b
Soumyadip Majumder <94290293+SoumyadipYT-OSS@users.noreply.github.com>
2023-09-02 12:19:43 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./RedeemCouponPage.html", "./RedeemCouponPage.js"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
